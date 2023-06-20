ef4b79024841b4274b4d7530b43a3e93c02f5e92
William Brown <willis291@gmail.com>
2023-06-20 00:24:42 -0600

    updated * path

--- server.js
+++ server.js
@@ -22,13 +22,9 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/public/pages/404.html"));
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Example app listening at http://localhost:${PORT}`);
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log(
