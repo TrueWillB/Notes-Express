e375c95a92d57127f4a2fda60f582e18562f84d2
William Brown <willis291@gmail.com>
2023-06-20 00:14:30 -0600

    got rid of * path

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/pages/404.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/pages/404.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
