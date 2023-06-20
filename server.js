26a9aefaa559060a33b37ab073238374966076e2
William Brown <willis291@gmail.com>
2023-06-20 00:20:40 -0600

    tried changing the port based on info I found online

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ app.get("/notes", (req, res) => {
 //   res.sendFile(path.join(__dirname, "/public/pages/404.html"));
 // });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Example app listening at http://localhost:${PORT}`);
+// });
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
